6c2a90724f5afd9df0eb1c59967b2283aaf5e6e3
James Tyrrell <james@floatdesign.net>
2018-10-13 12:52:46 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'James',
-    text: 'Hey, this is James'
-  });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -17,15 +17,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Mike',
-    text: 'Hey, what is going on?',
-    createdAt: new Date().getTime()
-  });
-
   // Event listeners
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
